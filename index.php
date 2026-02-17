bf8e5fe31aea63005dd1ea9ff83e62f03ba5625c
Miguel Caballero <mc.caballero95@gmail.com>
2026-02-16 20:32:00 -0500

    making a router

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 
 require "functions.php";
 
-$heading = "Home";
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"]; // Get the path from the URL, ignoring query parameters
 
-require "views/index.view.php";
+$routes = [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact" => "controllers/contact.php"
+];
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/$code.view.php";
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
